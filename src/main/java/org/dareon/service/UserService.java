502cc089eea9e49c8957e814bc2fe3e3cac442c2
Loy Larvin <s3562412@student.rmit.edu.au>
2017-04-19 14:02:52 +1000

    minor bug

--- src/main/java/org/dareon/service/UserService.java
+++ src/main/java/org/dareon/service/UserService.java
@@ -2,6 +2,8 @@ package org.dareon.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.dareon.domain.User;
 
 public interface UserService
@@ -9,5 +11,6 @@ public interface UserService
 
     public User findByEmail(String email);
     public List<User> list();
+    public User save(User user);
     
 }
