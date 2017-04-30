51599ed35e698a62ca70092fa9ef18c998b403c1
gaddirg <gaddi.rommel@gmail.com>
2017-04-30 19:38:59 +1000

    Updated to search using variables

--- maintenance/addNewSysAdmin.sql
+++ maintenance/addNewSysAdmin.sql
@@ -1,3 +1,18 @@
+#Add a new systems administrator account
+
+SET @email="sysadmin@rmit.edu.au", @password="password", 
+	@first_name="RMIT", @last_name="Systems Admin", @institution="RMIT University";
+
+/*--- do not edit after this line ------------------------------- */
 USE dareondb;
-INSERT INTO users VALUES(null,null,"sysadmin@rmit.edu.au","RMIT","RMIT University","Systems Admin","password");
-INSERT INTO users_roles values((select id from users where email = "sysadmin@rmit.edu.au"), 2);
+
+INSERT INTO users(email,password,first_name,last_name,institution) 
+	VALUES(@email,@password,@first_name,@last_name,@institution);
+
+INSERT INTO users_roles(user_id,role_id) 
+	VALUES((SELECT id FROM users WHERE email = @email), (SELECT id FROM roles WHERE name = "ROLE_SA"));
+
+SELECT email,password,first_name,last_name,institution,name AS role 
+	FROM users JOIN users_roles ON(users.id = users_roles.user_id)
+		JOIN roles ON(users_roles.role_id = roles.id)
+			WHERE email=@email;
