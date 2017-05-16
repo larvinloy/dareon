88a260b34823715035832216724123c6c30db1a2
gaddirg <gaddi.rommel@gmail.com>
2017-05-16 18:12:04 +1000

    Added list, edit, delete functionality for Classifications

--- src/main/java/org/dareon/controller/FORController.java
+++ src/main/java/org/dareon/controller/FORController.java
@@ -1,37 +1,19 @@
 package org.dareon.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
-import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
+import java.util.List;
 
 import org.dareon.domain.FOR;
-import org.dareon.domain.CFP;
-import org.dareon.domain.Repo;
-import org.dareon.domain.User;
 import org.dareon.json.JsonFORTree;
 import org.dareon.service.FORService;
-import org.dareon.service.CFPService;
-import org.dareon.service.RepoService;
-import org.dareon.service.UserDetailsImpl;
-import org.dareon.service.UserService;
 import org.dareon.wrappers.FORForm;
-import org.dareon.wrappers.RepoForm;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ServerProperties.Session;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.security.authentication.*;
@@ -40,13 +22,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class FORController
@@ -72,13 +49,12 @@ public class FORController
 
     // @PreAuthorize("hasAuthority('REPO_CREATE_PRIVILEGE')")
     @RequestMapping("/for/create")
-    public String repoCreate(Model model) throws JSONException
+    public String forCreate(Model model) throws JSONException
     {
 	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 	JSONObject obj = new JSONObject();
 	JSONArray arr = new JSONArray();
-	// return aNZSRCService.list();
-	// for(FOR a : fORService.listByLevel(levelService.findById((long)1)))
+
 	for (FOR a : fORService.list())
 	{
 	    obj.put("text", a.getCode() + " | " + a.getName());
@@ -96,11 +72,12 @@ public class FORController
 	model.addAttribute("parent", new String());
 
 	return "for/create";
-
     }
 
+
+    
     @RequestMapping(value = "/for/create", method = RequestMethod.POST)
-    public String fORSave(@ModelAttribute FORForm fORForm)
+    public String forSave(@ModelAttribute FORForm fORForm)
     {
 	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
@@ -108,8 +85,6 @@ public class FORController
 	{
 	    FOR newFOR = fORForm.getFoR();
 	    FOR savedNewFOR = fORService.save(newFOR);
-	    System.out.println("Child ===============>" + newFOR.getCode() + " " + newFOR.getCode() + " "
-		    + newFOR.getName() + " " + " " + newFOR.getParent());
 	} 
 	else
 	{
@@ -123,26 +98,58 @@ public class FORController
 	    parent.setChildren(newChildren);
 	    newFOR.setParent(parent);
 
-	    //
 	    FOR savedNewFOR = fORService.save(newFOR);
-	    FOR savedParent = fORService.save(fORService.findById((long) p));
-
-	    System.out.println("Parent ===============>" + parent.getId() + " " + parent.getCode() + " "
-		    + parent.getName() + " " + " " + parent.getChildren());
-	    System.out.println("Child ===============>" + newFOR.getCode() + " " + newFOR.getCode() + " "
-		    + newFOR.getName() + " " + " " + newFOR.getParent());
+	    FOR savedParent = fORService.save(parent);
 	}
 
-	return "index";
+	return "redirect:list";
     }
 
+    
+
     @RequestMapping("/for/edit/{id}")
-    public String fOREdit(@PathVariable Long id, Model model)
+    public String forEdit(@PathVariable Long id, Model model)
     {
 	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	// model.addAttribute("fOR", new FOR());
 	FORForm fORForm = new FORForm();
+	fORForm.setFoR(fORService.findById(id));
+	
+	JSONObject obj = new JSONObject();
+	JSONArray arr = new JSONArray();
 
+	for (FOR a : fORService.list())
+	{
+	    obj.put("text", a.getCode() + " | " + a.getName());
+	    obj.put("id", a.getId());
+	    obj.put("tags", new JSONArray().put(String.valueOf(a.getChildren().size())));
+	    if (a.getChildren().size() > 0)
+		obj = JsonFORTree.addChildren(obj, a.getChildren());
+	    arr.put(obj);
+	    obj = new JSONObject();
+	}
+	model.addAttribute("message", arr.toString());
+	
+	JSONArray pre = new JSONArray();
+
+	if(fORService.findById(id).getParent() != null)
+		pre.put(fORService.findById(id).getParent().getId().toString());
+
+	fORForm.setPre(pre.toString());
+	model.addAttribute("fORForm", fORForm);
+
+	return "for/create";
+    }
+    
+    
+    
+    
+    
+    
+    
+    @RequestMapping("/for/list")
+    public String forList(Model model) throws JSONException
+    {
+	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 	JSONObject obj = new JSONObject();
 	JSONArray arr = new JSONArray();
 
@@ -157,18 +164,23 @@ public class FORController
 	    obj = new JSONObject();
 	}
 
+	model.addAttribute("fORForm", new FORForm());
 	model.addAttribute("message", arr.toString());
+	//model.addAttribute("pre", new String());
 
-	JSONArray pre = new JSONArray();
+	return "for/list";
 
-	pre.put(fORService.findById(id).getParent().getId().toString());
-
-	fORForm.setPre(pre.toString());
-	fORForm.setFoR(fORService.findById(id));
-	model.addAttribute("FORForm", fORForm);
-	System.out.println("PRE =====>" + pre);
-	System.out.println("FOR =====>" + fORForm.getFoR().getName());
-	return "for/create";
     }
+    
+    
+    
+    @RequestMapping("/for/deleteconfirmed/{id}")
+    public RedirectView forDeleteConfirmed(@PathVariable Long id, Model model)
+    {
+	fORService.delete(id);
+	 return new RedirectView("/for/list");
+    }
+    
+    
 
 }
