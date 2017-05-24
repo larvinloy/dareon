cfa910e5a3600163600cdc41b2ac4c1177ad500e
Loy Larvin <s3562412@student.rmit.edu.au>
2017-05-24 16:06:01 +1000

    Retyring auto upload to git

--- upload-jar.sh
+++ upload-jar.sh
@@ -11,10 +11,7 @@ rm -f docker-dareon/DareonWebApp-0.0.1-SNAPSHOT.jar
 cp target/DareonWebApp-0.0.1-SNAPSHOT.jar docker-dareon/
 #add, commit and push files
 
-git add -f .
-git remote rm origin
-git remote add origin https://larvinloy:$GITHUB_API_KEY@github.com/larvinloy/docker-dareon.git
-git add -f .
+git add .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
 git push -fq origin master
 echo -e "Done\n"
