402f155010b54e2ccb5c537145c4297697d31993
Loy Larvin <s3562412@student.rmit.edu.au>
2017-05-24 15:41:07 +1000

    Trying aut push to docker repo

--- upload-jar.sh
+++ upload-jar.sh
@@ -1,19 +1,14 @@
-#create a new directory that will contain out generated apk 
-mkdir $HOME/buildjar/ 
-#copy generated apk from build folder to the folder just created
 
-cp -R app/build/outputs/apk/app-debug.apk $HOME/android/
-#go to home and setup git  
-
-cd $HOME
+#setup git  
 git config --global user.email "s3562412@student.rmit.edu.au"
 git config --global user.name "Loy Larvin" 
-#clone the repository in the buildApk folder
 
-git clone --quiet --branch=master  https://user-name:$GITHUB_API_KEY@github.com/user-name/docker-dareon  master > /dev/null
+#clone the docker repository
+git clone --quiet --branch=master  https://user-name:$GITHUB_API_KEY@github.com/user-name/docker-dareon
 #go into directory and copy data we're interested
 
-cd master  cp -Rf $HOME/android/* .
+rm -f docker-dareon/DareonWebApp-0.0.1-SNAPSHOT.jar
+cp target/DareonWebApp-0.0.1-SNAPSHOT.jar docker-dareon/
 #add, commit and push files
 
 git add -f .
@@ -21,5 +16,5 @@ git remote rm origin
 git remote add origin https://user-name:$GITHUB_API_KEY@github.com/user-name/repo-name.git
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
-git push -fq origin master > /dev/null
+git push -fq origin master
 echo -e "Done\n"
